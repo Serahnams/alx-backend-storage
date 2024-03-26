d001f30e341d96f8a0a0a45f2fe3fa1dea6b116c
Serahnams <serahnams@gmail.com>
2024-03-26 21:16:23 +0000

    Correction Task 12

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,45 +1,35 @@
 #!/usr/bin/env python3
-"""
-script that provides some stats about Nginx logs stored in MongoDB
-"""
+""" Python Module """
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-    # get number of documents in collection
-    docs_num = nginx_logs.count_documents({})
-    get_num = nginx_logs.count_documents({'method': 'GET'})
-    post_num = nginx_logs.count_documents({'method': 'POST'})
-    put_num = nginx_logs.count_documents({'method': 'PUT'})
-    patch_num = nginx_logs.count_documents({'method': 'PATCH'})
-    delete_num = nginx_logs.count_documents({'method': 'DELETE'})
-    get_status = nginx_logs.count_documents({'method': 'GET',
-        'path': '/status'})
-    IPs_count = nginx_logs.aggregate([
-        {
-            '$group': {
-                '_id': "$ip",
-                'count': {'$sum': 1}
-                }
-            },
-        }
-        "$sort": {"count": -1}
-        }
-    ])
-    print("{} logs".format(docs_num))
-    print("Methods:")
-    print("\tmethod GET: {}".format(get_num))
-    print("\tmethod POST: {}".format(post_num))
-    print("\tmethod PUT: {}".format(put_num))
-    print("\tmethod PATCH: {}".format(patch_num))
-    print("\tmethod DELETE: {}".format(delete_num))
-    print("{} status check".format(get_status))
-    print("IPs:")
-    x = 0
-    for i in IPs_count:
-        print("\t{}: {}".format(i.get('_id'), i.get('count')))
-        x += 1
-        if x > 9:
-            break
+        """ Improve 12-log_stats.py by adding the top 10 of the most present IPs in
+        the collection nginx of the database logs """
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        col = client.logs.nginx
+        print("{} logs".format(col.estimated_document_count()))
+        print("Methods:")
+        for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+            count = col.count_documents({'method': method })
+            print("\tmethod {}: {}".format(method, count))
+        status_get = col.count_documents({'method': 'GET', 'path': "/status"})
+        print("{} status check".format(status_get))
+        print("IPs:")
+        topIps = col.aggregate([
+            {"$group":
+                {
+                    "_id": "$ip",
+                    "count": {"$sum": 1}
+                    }
+                },
+            {"$sort": {"count": -1}},
+            {"$limit": 10},
+            {"$project": {
+                "_id": 0,
+                "ip": "$_id",
+                "count": 1
+                }}
+            ])
+        for ip in topIps:
+            print("\t{}: {}".format(ip.get('ip'), ip.get('count')))
